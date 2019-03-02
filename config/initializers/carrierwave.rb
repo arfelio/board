0773db03dfa305ef4e4e25dde38f4a2c0038f195
Artem <arfelio1980@gmail.com>
2019-03-02 23:33:11 +0200

    fix advertisements request spec

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-if Rails.env.test? or Rails.env.cucumber?
+if Rails.env.test? or Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
